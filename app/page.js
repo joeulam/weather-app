dce38ed9c6301a5c7e3add380ee5f249c80045f3
Joey Lam <joeulam345@gmail.com>
2023-06-10 20:20:25 -0400

    Added head

--- app/page.js
+++ app/page.js
@@ -1,9 +1,11 @@
 import Image from 'next/image'
-
+import Head from 'next/head'
 export default function Home() {
   return (
     <div>
-      
+      <Head>
+        <title>Joey's Weather app</title>
+      </Head>
     </div>
   )
 }
